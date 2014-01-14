36a8245b22f7f0661fd90a8a928ea7cff804704b
Jeremiah Snapp <jeremiah@getchef.com>
2014-01-14 13:02:54 -0500

    Add default byobu config

--- recipes/helpful-addons.rb
+++ recipes/helpful-addons.rb
@@ -6,6 +6,15 @@ package 'vim-nox'
 # byobu installs and enhances screen and tmux terminal multiplexers
 package 'byobu'
 
+directory '/root/.byobu' do
+  recursive true
+end
+
+cookbook_file '/root/.byobu/.tmux.conf' do
+  source 'tmux.conf'
+  action :create_if_missing
+end
+
 cookbook_file '/etc/profile.d/lxc-helpers.sh' do
   source 'lxc-helpers.sh'
 end
