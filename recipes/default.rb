7b7e6b0e3e5f4a7a245cda3887ddc04a390989ea
Jeremiah Snapp <jeremiah@getchef.com>
2014-01-17 16:05:35 -0500

    Move helpful LXC stuff into separate lxc-helpful-addons recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,8 +2,10 @@ include_recipe 'apt'
 
 include_recipe 'dev-lxc::byobu'
 
+include_recipe 'dev-lxc::helpful-addons'
+
 include_recipe 'dev-lxc::lxc'
 
-include_recipe 'dev-lxc::ruby-lxc'
+include_recipe 'dev-lxc::lxc-helpful-addons'
 
-include_recipe 'dev-lxc::helpful-addons'
+include_recipe 'dev-lxc::ruby-lxc'
--- recipes/helpful-addons.rb
+++ recipes/helpful-addons.rb
@@ -2,7 +2,3 @@ package 'curl'
 
 package 'emacs24-nox'
 package 'vim-nox'
-
-cookbook_file '/etc/profile.d/lxc-helpers.sh' do
-  source 'lxc-helpers.sh'
-end
