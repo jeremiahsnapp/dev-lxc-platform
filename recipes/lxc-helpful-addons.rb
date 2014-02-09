131f2e69aed2541c7ed9a8be698129550052e4b2
Jeremiah Snapp <jeremiah@getchef.com>
2014-02-09 05:52:41 -0500

    No longer use dhcp-hosts.conf to set static IP
    For simplicity sake I now define static ip's in a yaml file along
    with the rest of the containers config.

--- recipes/lxc-helpful-addons.rb
+++ recipes/lxc-helpful-addons.rb
@@ -15,12 +15,6 @@ end
 
 directory '/var/lib/lxc/dnsmasq'
 
-cookbook_file '/var/lib/lxc/dnsmasq/dhcp-hosts.conf' do
-  source 'dhcp-hosts.conf'
-  action :create_if_missing
-  notifies :restart, 'service[lxc-net]'
-end
-
 cookbook_file '/var/lib/lxc/dnsmasq/addn-hosts.conf' do
   source 'addn-hosts.conf'
   action :create_if_missing
