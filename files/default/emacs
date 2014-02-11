7cd98df5645a4b898646f1ac63a64569f54d292e
Jeremiah Snapp <jeremiah@getchef.com>
2014-02-11 13:25:09 -0500

    Disable emacs auto-save and backups for root user
    The auto backup of files in particular was creating problems
    with dnsmasq when backups of dhcp-hosts.conf and of the leases file
    existed. Dnsmasq was actually usng the contents of the backups
    in addition to the originals.

---
+++ files/default/emacs
@@ -0,0 +1,4 @@
+;disable backup
+(setq backup-inhibited t)
+;disable auto save
+(setq auto-save-default nil)
--- recipes/helpful-addons.rb
+++ recipes/helpful-addons.rb
@@ -1,4 +1,9 @@
 package 'curl'
 
 package 'emacs24-nox'
+cookbook_file '/root/.emacs' do
+  source 'emacs'
+  action :create_if_missing
+end
+
 package 'vim-nox'
