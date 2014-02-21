ca8a40030f18d581d788671571624a56d428d0d0
Jeremiah Snapp <jeremiah@getchef.com>
2014-02-21 12:42:32 -0500

    ruby-lxc released to rubygems.org

--- recipes/ruby-lxc.rb
+++ recipes/ruby-lxc.rb
@@ -9,19 +9,6 @@ include_recipe "rbenv::ruby_build"
 
 rbenv_ruby "2.1.0"
 
-git '/usr/local/src/ruby-lxc' do
-  repository 'https://github.com/lxc/ruby-lxc.git'
-end
-
-rbenv_execute 'gem-build-ruby-lxc' do
+rbenv_gem 'ruby-lxc' do
   ruby_version '2.1.0'
-  cwd '/usr/local/src/ruby-lxc'
-  command 'gem build ruby-lxc.gemspec'
-  creates 'ruby-lxc-0.1.0.gem'
-end
-
-gem_package 'ruby-lxc' do
-  gem_binary '/opt/rbenv/versions/2.1.0/bin/gem'
-  source '/usr/local/src/ruby-lxc/ruby-lxc-0.1.0.gem'
-  options '--no-ri --no-rdoc'
 end
