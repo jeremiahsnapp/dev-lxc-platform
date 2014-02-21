8732b88a217290436ec3d91023b6bac7d70d99bf
Jeremiah Snapp <jeremiah@getchef.com>
2014-02-21 13:12:25 -0500

    Set global ruby version

--- recipes/ruby-lxc.rb
+++ recipes/ruby-lxc.rb
@@ -7,7 +7,9 @@ node.default['rbenv']['group_users'] = ['root', 'vagrant']
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
-rbenv_ruby "2.1.0"
+rbenv_ruby "2.1.0" do
+  global true
+end
 
 rbenv_gem 'ruby-lxc' do
   ruby_version '2.1.0'
