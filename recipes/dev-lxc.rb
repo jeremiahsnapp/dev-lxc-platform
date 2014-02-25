4b112c7c8764610d72eeddfa93f1cab322aea0cf
Jeremiah Snapp <jeremiah@getchef.com>
2014-02-25 12:55:43 -0500

    Install dev-lxc gem from github source

--- recipes/dev-lxc.rb
+++ recipes/dev-lxc.rb
@@ -15,6 +15,19 @@ rbenv_gem 'ruby-lxc' do
   ruby_version '2.1.0'
 end
 
-rbenv_gem 'dev-lxc' do
+git '/usr/local/src/dev-lxc' do
+  repository 'https://github.com/jeremiahsnapp/dev-lxc.git'
+end
+
+rbenv_execute 'gem-build-dev-lxc' do
   ruby_version '2.1.0'
+  cwd '/usr/local/src/dev-lxc'
+  command 'gem build dev-lxc.gemspec'
+  creates 'dev-lxc-0.1.0.gem'
+end
+
+gem_package 'gem-lxc' do
+  gem_binary '/opt/rbenv/versions/2.1.0/bin/gem'
+  source '/usr/local/src/dev-lxc/dev-lxc-0.1.0.gem'
+  options '--no-ri --no-rdoc'
 end
