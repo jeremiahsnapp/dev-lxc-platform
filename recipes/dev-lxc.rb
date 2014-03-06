839468ee82f46244eeb1e55b06a2878f2b633240
Jeremiah Snapp <jeremiah@getchef.com>
2014-03-06 13:30:06 -0500

    Alias dl to dev-lxc

--- recipes/dev-lxc.rb
+++ recipes/dev-lxc.rb
@@ -14,3 +14,11 @@ package 'lxc-dev'
 rbenv_gem 'dev-lxc' do
   ruby_version '2.1.0'
 end
+
+ruby_block "alias dev-lxc" do
+  block do
+    rc = Chef::Util::FileEdit.new("/root/.bashrc")
+    rc.insert_line_if_no_match(/^alias dl=dev-lxc$/, "alias dl=dev-lxc")
+    rc.write_file
+  end
+end
