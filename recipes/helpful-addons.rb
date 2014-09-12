c458c1a0ed20277855e40768224aa2ac6936298a
Jeremiah Snapp <jeremiah@getchef.com>
2014-09-12 15:38:51 -0400

    Specify emacs package per platform_version

--- recipes/helpful-addons.rb
+++ recipes/helpful-addons.rb
@@ -4,7 +4,14 @@ package 'tree'
 
 package 'htop'
 
-package 'emacs24-nox'
+if node['platform'] == 'ubuntu'
+  case node['platform_version'].to_f
+  when 14.04
+    package 'emacs24-nox'
+  when 12.04
+    package 'emacs23-nox'
+  end
+end
 cookbook_file '/root/.emacs' do
   source 'emacs'
   action :create_if_missing
