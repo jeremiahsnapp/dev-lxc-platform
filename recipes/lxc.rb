6bea60b8dc47ca29530fbecaaba204d1eb6012d5
Jeremiah Snapp <jeremiah@getchef.com>
2014-09-12 05:33:02 -0400

    Use ubuntu-lxc apt_repo for Ubuntu 12.04

--- recipes/lxc.rb
+++ recipes/lxc.rb
@@ -1,3 +1,13 @@
+if node['platform'] == 'ubuntu' && node['platform_version'].to_f == 12.04
+  apt_repository 'ubuntu-lxc' do
+    uri 'http://ppa.launchpad.net/ubuntu-lxc/stable/ubuntu'
+    distribution node['lsb']['codename']
+    components ['main']
+    keyserver 'keyserver.ubuntu.com'
+    key '7635B973'
+  end
+end
+
 package 'lxc' do
   action :upgrade
 end
