b80fff03d68a1e25c0582653b3027b37abc193cc
Jeremiah Snapp <jeremiah@getchef.com>
2015-04-02 15:35:17 -0400

    Add tls and tks aliases
    These aliases make it easier to list and kill tmux sessions.

--- recipes/byobu.rb
+++ recipes/byobu.rb
@@ -14,3 +14,19 @@ cookbook_file '/root/.byobu/.tmux.conf' do
   source 'tmux.conf'
   action :create_if_missing
 end
+
+ruby_block "alias tls" do
+  block do
+    rc = Chef::Util::FileEdit.new("/root/.bashrc")
+    rc.insert_line_if_no_match(/^alias tls=/, "alias tls='tmux ls'")
+    rc.write_file
+  end
+end
+
+ruby_block "alias tks" do
+  block do
+    rc = Chef::Util::FileEdit.new("/root/.bashrc")
+    rc.insert_line_if_no_match(/^alias tks=/, "alias tks='tmux kill-session -t'")
+    rc.write_file
+  end
+end
