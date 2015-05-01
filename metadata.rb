7cb6874e59e8801c965a0d2db420ca94d16d433b
Jeremiah Snapp <jeremiah@getchef.com>
2015-05-01 10:51:40 -0400

    Bump to version 1.2.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'dev-lxc-platform'
 license           'Apache 2.0'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.1.0'
+version           '1.2.0'
 
 supports 'ubuntu'
 
