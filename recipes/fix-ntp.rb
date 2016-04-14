8052156258fca8f638577695d43f3bd6f1b0b852
Jeremiah Snapp <jeremiah@chef.io>
2016-04-14 11:57:35 -0400

    Require Ubuntu 15.10 host VM

--- recipes/fix-ntp.rb
+++
@@ -1,5 +0,0 @@
-# ref: https://github.com/gmiranda23/ntp/issues/105#issuecomment-99144029
-# ref: https://github.com/gmiranda23/ntp/issues/108
-if node['platform'] == 'ubuntu' && node['platform_version'].to_f >= 15.04
-  resources("service[ntp]").provider Chef::Provider::Service::Init::Debian
-end
--- recipes/helpful-addons.rb
+++ recipes/helpful-addons.rb
@@ -4,14 +4,7 @@ package 'tree'
 
 package 'htop'
 
-if node['platform'] == 'ubuntu'
-  case node['platform_version'].to_f
-  when 14.04, 15.04
-    package 'emacs24-nox'
-  when 12.04
-    package 'emacs23-nox'
-  end
-end
+package 'emacs24-nox'
 cookbook_file '/root/.emacs' do
   source 'emacs'
   action :create_if_missing
