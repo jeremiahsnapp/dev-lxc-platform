fab4646b522ce33d84535fc6a9655dc1ede9361f
Jeremiah Snapp <jeremiah@chef.io>
2016-04-14 12:12:22 -0400

    Uninstall system ruby

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,11 @@ include_recipe 'dev-lxc-platform::byobu'
 
 include_recipe 'dev-lxc-platform::helpful-addons'
 
+apt_package 'ruby' do
+  options '--auto-remove'
+  action :purge
+end
+
 execute "Setup ChefDK as default ruby" do
   command "chef shell-init bash >> /root/.bashrc"
   user "root"
