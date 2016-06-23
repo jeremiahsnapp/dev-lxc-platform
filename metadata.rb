5ac6acb244b8d9147c6a12f7270e9668a64b230a
Jeremiah Snapp <jeremiah@chef.io>
2016-06-23 11:07:53 -0400

    Bump cookbook version to 4.0.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'dev-lxc-platform'
 license           'Apache 2.0'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '3.1.2'
+version           '4.0.0'
 
 supports 'ubuntu'
 
