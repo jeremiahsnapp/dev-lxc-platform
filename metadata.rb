2fac3aa4d7e972edbf778e918d987e42e6a2ee62
Jeremiah Snapp <jeremiah@chef.io>
2016-07-20 09:16:02 -0400

    Bump version to 4.0.2

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'dev-lxc-platform'
 license           'Apache 2.0'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '4.0.1'
+version           '4.0.2'
 
 supports 'ubuntu'
 
