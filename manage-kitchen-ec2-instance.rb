11ffd9415eab3924347bf2dd82bc0e7fbee67f79
Jeremiah Snapp <jeremiah@chef.io>
2017-01-20 05:35:55 -0500

    Refactor manage-kitchen-ec2-instance.rb

--- manage-kitchen-ec2-instance.rb
+++ manage-kitchen-ec2-instance.rb
@@ -7,19 +7,19 @@ require 'thor'
 class ManageEC2Instance < Thor
 
   no_commands{
-    def get_instance_id(kitchen_instance_file=nil)
+    def get_instance(kitchen_instance_file=nil)
       if ! File.exists?(kitchen_instance_file)
         puts "ERROR: Kitchen instance file '#{kitchen_instance_file}' does not exist."
         exit 1
       end
       kitchen_instance = YAML.load(IO.read(kitchen_instance_file))
-      kitchen_instance['server_id']
+      Aws::EC2::Instance.new(kitchen_instance['server_id'])
     end
   }    
 
   desc "start [KITCHEN_INSTANCE_FILE]", "Start instance"
   def start(kitchen_instance_file)
-    i = Aws::EC2::Instance.new(get_instance_id(kitchen_instance_file))
+    i = get_instance(kitchen_instance_file)
     puts "Starting instance #{i.id}"
     i.start
     i.wait_until_running
@@ -27,7 +27,7 @@ class ManageEC2Instance < Thor
 
   desc "stop [KITCHEN_INSTANCE_FILE]", "Stop instance"
   def stop(kitchen_instance_file)
-    i = Aws::EC2::Instance.new(get_instance_id(kitchen_instance_file))
+    i = get_instance(kitchen_instance_file)
     puts "Stopping instance #{i.id}"
     i.stop
     i.wait_until_stopped
@@ -35,7 +35,7 @@ class ManageEC2Instance < Thor
 
   desc "update-hostname [KITCHEN_INSTANCE_FILE]", "Update hostname in kitchen instance file"
   def update_hostname(kitchen_instance_file)
-    i = Aws::EC2::Instance.new(get_instance_id(kitchen_instance_file))
+    i = get_instance(kitchen_instance_file)
     puts "Updating hostname for instance #{i.id} to #{i.public_dns_name}"
     kitchen_instance = YAML.load(IO.read(kitchen_instance_file))
     kitchen_instance['hostname'] = i.public_dns_name
