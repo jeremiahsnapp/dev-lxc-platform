faea817c1f0e0eacc11c76b489b74bf2f390676d
Jeremiah Snapp <jeremiah@chef.io>
2017-01-23 09:50:20 -0500

    Install mitmproxy

--- recipes/helpful-addons.rb
+++ recipes/helpful-addons.rb
@@ -11,3 +11,13 @@ cookbook_file '/root/.emacs' do
 end
 
 package 'vim-nox'
+
+remote_file "#{Chef::Config['file_cache_path']}/mitmproxy-1.0.2-linux.tar.gz" do
+  source 'https://github.com/mitmproxy/mitmproxy/releases/download/v1.0.2/mitmproxy-1.0.2-linux.tar.gz'
+  notifies :run, 'bash[extract mitmproxy]', :immediately
+end
+
+bash 'extract mitmproxy' do
+  code "tar xzf #{Chef::Config['file_cache_path']}/mitmproxy-1.0.2-linux.tar.gz -C /usr/local/bin"
+  action :nothing
+end
