f123ee1d11b6fc6295ef6f36f72a9a1559281302
Jeremiah Snapp <jeremiah@chef.io>
2017-02-07 08:52:01 -0500

    Restart services immediately. They might be needed later in the chef-client run

--- recipes/lxc-helpful-addons.rb
+++ recipes/lxc-helpful-addons.rb
@@ -4,24 +4,24 @@ end
 
 cookbook_file '/etc/default/lxc-net' do
   source 'lxc-net'
-  notifies :restart, 'service[lxc-net]'
+  notifies :restart, 'service[lxc-net]', :immediately
 end
 
 cookbook_file '/etc/lxc/dnsmasq.conf' do
   source 'dnsmasq.conf'
-  notifies :restart, 'service[lxc-net]'
+  notifies :restart, 'service[lxc-net]', :immediately
 end
 
 cookbook_file '/etc/lxc/dhcp-hosts.conf' do
   source 'dhcp-hosts.conf'
   action :create_if_missing
-  notifies :restart, 'service[lxc-net]'
+  notifies :restart, 'service[lxc-net]', :immediately
 end
 
 cookbook_file '/etc/lxc/addn-hosts.conf' do
   source 'addn-hosts.conf'
   action :create_if_missing
-  notifies :restart, 'service[lxc-net]'
+  notifies :restart, 'service[lxc-net]', :immediately
 end
 
 # restarting systemd-resolved.service updates /etc/resolv.conf AND /run/systemd/resolve/resolv.conf
@@ -31,7 +31,7 @@ end
 
 cookbook_file '/etc/resolvconf/resolv.conf.d/head' do
   source 'resolvconf-head'
-  notifies :restart, 'service[systemd-resolved.service]'
+  notifies :restart, 'service[systemd-resolved.service]', :immediately
 end
 
 directory '/usr/local/share/lxc/hooks' do
