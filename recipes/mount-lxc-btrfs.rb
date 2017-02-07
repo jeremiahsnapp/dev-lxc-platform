9ffc70501043c672eacb1197ecd7ee81d8fc1b18
Jeremiah Snapp <jeremiah@chef.io>
2017-02-07 14:51:49 -0500

    Don't mount btrfs on /var/cache/lxc
    This really doesn't provide any benefit.

--- recipes/mount-lxc-btrfs.rb
+++ recipes/mount-lxc-btrfs.rb
@@ -8,14 +8,3 @@ mount '/var/lib/lxc' do
   action [:mount, :enable]
   not_if "mount -t btrfs | grep -q ' on /var/lib/lxc .*,subvol=/lxclib'"
 end
-
-directory '/btrfs/lxccache'
-directory '/var/cache/lxc'
-mount '/var/cache/lxc' do
-  device '/btrfs/lxccache'
-  fstype 'none'
-  options 'bind'
-  pass 0
-  action [:mount, :enable]
-  not_if "mount -t btrfs | grep -q ' on /var/cache/lxc .*,subvol=/lxccache'"
-end
