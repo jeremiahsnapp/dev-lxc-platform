d60b91613b4dbb3509801ff2569d8d9aadf38924
Jeremiah Snapp <jeremiah@chef.io>
2017-02-10 16:26:43 -0500

    Fix sysdig install bug

--- recipes/sysdig.rb
+++ recipes/sysdig.rb
@@ -3,3 +3,8 @@ include_recipe 'apt'
 package "linux-headers-#{node['kernel']['release']}"
 
 include_recipe 'sysdig'
+# the following is necessary because of this bug in the sysdig cookbook
+# https://github.com/jarosser06/chef-sysdig/issues/12
+edit_resource(:apt_repository, 'sysdig') do
+  distribution false
+end
