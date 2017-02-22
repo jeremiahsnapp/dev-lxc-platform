2798fe60e6ab07ba6b3d1e3af869813ff9f8ad05
Jeremiah Snapp <jeremiah@chef.io>
2017-02-22 00:38:44 -0500

    apt_repository can't identify key fingerprints when gnupg 2.1.x is used
    Monkey patch until the following Chef Client bug gets fixed
    https://github.com/chef/chef/issues/5831

---
+++ libraries/apt_repository_fix.rb
@@ -0,0 +1,26 @@
+# this monkey patch is in place until the following Chef Client bug gets fixed
+# https://github.com/chef/chef/issues/5831
+
+class Chef
+  class Provider
+    class AptRepository
+
+      def extract_fingerprints_from_cmd(cmd)
+        so = shell_out(cmd)
+        so.run_command
+        so.stdout.split(/\n/).map do |t|
+          if z = t.match(/^fpr:+:([0-9A-F]+):/)
+            z[1].split.join
+          end
+        end.compact
+      end
+
+      def no_new_keys?(file)
+        installed_keys = extract_fingerprints_from_cmd("apt-key adv --list-public-keys --with-fingerprint --with-colons")
+        proposed_keys = extract_fingerprints_from_cmd("gpg --with-fingerprint --with-colons #{file}")
+        (installed_keys & proposed_keys).sort == proposed_keys.sort
+      end
+
+    end
+  end
+end
