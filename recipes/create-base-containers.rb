c9e06feec518a36a6e0b686ec65a65bcd17fc086
Jeremiah Snapp <jeremiah@chef.io>
2017-02-28 10:14:48 -0500

    Properly specify root user in the 'Create base container' execute resource

--- recipes/create-base-containers.rb
+++ recipes/create-base-containers.rb
@@ -1,6 +1,8 @@
 node['dev-lxc-platform']['base_containers'].each do |bc|
   execute "Create base container #{bc}" do
-    command "sudo -i chef exec dev-lxc create #{bc}"
+    command "chef exec dev-lxc create #{bc}"
+    user 'root'
+    environment({ 'HOME' => '/root' })
     not_if "lxc-info -n #{bc}"
   end
 end
