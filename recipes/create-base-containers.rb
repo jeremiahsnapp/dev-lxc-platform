6d57dcf90243b360a4816d24605309aef5f545f4
Jeremiah Snapp <jeremiah@chef.io>
2017-03-06 14:04:44 -0500

    Use dl command in create-base-container recipe

--- recipes/create-base-containers.rb
+++ recipes/create-base-containers.rb
@@ -1,6 +1,6 @@
 node['dev-lxc-platform']['base_containers'].each do |bc|
   execute "Create base container #{bc}" do
-    command "chef exec dev-lxc create #{bc}"
+    command "chef exec dl create #{bc}"
     user 'root'
     environment({ 'HOME' => '/root' })
     not_if "lxc-info -n #{bc}"
