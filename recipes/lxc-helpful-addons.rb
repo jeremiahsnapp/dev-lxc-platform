325e6674a15d098a8a4e1aae161cba1ff7ddb062
Jeremiah Snapp <jeremiah@chef.io>
2017-08-16 12:02:56 -0400

    always restart lxc-net instead of using `notifies`
    The lxc-net service should be restarted ASAP as opposed
    to waiting until the end of the chef client run
    but using `notifies :immediately` was causing problems
    during the first chef client run because some required
    files weren't generated yet

--- recipes/lxc-helpful-addons.rb
+++ recipes/lxc-helpful-addons.rb
@@ -4,24 +4,25 @@ end
 
 cookbook_file '/etc/default/lxc-net' do
   source 'lxc-net'
-  notifies :restart, 'service[lxc-net]', :immediately
 end
 
 cookbook_file '/etc/lxc/dnsmasq.conf' do
   source 'dnsmasq.conf'
-  notifies :restart, 'service[lxc-net]', :immediately
 end
 
 cookbook_file '/etc/lxc/dhcp-hosts.conf' do
   source 'dhcp-hosts.conf'
   action :create_if_missing
-  notifies :restart, 'service[lxc-net]', :immediately
 end
 
 cookbook_file '/etc/lxc/addn-hosts.conf' do
   source 'addn-hosts.conf'
   action :create_if_missing
-  notifies :restart, 'service[lxc-net]', :immediately
+end
+
+# restart lxc-net
+service 'lxc-net' do
+  action :restart
 end
 
 # restarting systemd-resolved.service updates /etc/resolv.conf AND /run/systemd/resolve/resolv.conf
