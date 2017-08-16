ac365dc18e5e5e919c43adce1f99e3074323ea16
Jeremiah Snapp <jeremiah@chef.io>
2017-08-16 15:13:05 -0400

    Add tinyproxy systemd unit file that restarts the service if it fails

--- recipes/tinyproxy.rb
+++ recipes/tinyproxy.rb
@@ -1,11 +1,15 @@
 package 'tinyproxy'
 
+cookbook_file '/etc/systemd/system/tinyproxy.service' do
+  source 'tinyproxy.service'
+  notifies :restart, 'service[tinyproxy]'
+end
+
 cookbook_file '/etc/tinyproxy.conf' do
   source 'tinyproxy.conf'
   notifies :restart, 'service[tinyproxy]'
 end
 
 service 'tinyproxy' do
-  provider Chef::Provider::Service::Systemd
   action [:enable, :start]
 end
